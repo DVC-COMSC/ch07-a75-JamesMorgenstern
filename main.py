ffcfbe17059f6dbc4b013c91b99bbf969aeae684
JamesMorgenstern <jamesklm@comcast.net>
2022-10-10 13:31:22 -0700

    committed1

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ num2 = list(map(int, input().split()))
 if len(num1) > len(num2):
 	print ('False')
 	sys.exit(0)
-# ******************************
-# Make your Code
-# ******************************
 
-# print ('True') or ('False')
+size = len(num1)
+check = False
+
+if (set(num1).issubset(num2)):
+	for i in range(len(num2)):
+		for j in range(len(num1)):
+			if (num2[i] != num1[j]):
+				break
+			i += 1
+			if (i == len(num1)):
+				check = True
+	print(check)
+else:
+	print(check)
